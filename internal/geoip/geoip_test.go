5800eadce368636ab17f4c426ad290f242fe6c01
Leigh MacDonald <leigh.macdonald@gmail.com>
2025-09-21 23:33:32 -0600

    Add external ip resolution api call for log_forwarding

--- internal/geoip/geoip_test.go
+++ internal/geoip/geoip_test.go
@@ -8,13 +8,13 @@ import (
 )
 
 func TestLookup(t *testing.T) {
-	record, errLookip := geoip.Lookup("12.55.66.88")
+	record, errLookip := geoip.Lookup(t.Context(), "12.55.66.88")
 	require.NoError(t, errLookip)
 	require.Equal(t, "US", record.Country.ISOCode)
 
-	_, errLookupDNS := geoip.Lookup("google.com")
+	_, errLookupDNS := geoip.Lookup(t.Context(), "google.com")
 	require.NoError(t, errLookupDNS)
 
-	_, err := geoip.Lookup("bad")
+	_, err := geoip.Lookup(t.Context(), "bad")
 	require.Error(t, err)
 }
