05cdf52ffaf9c7225c1acc117c58ef55fb34b92b
Leigh MacDonald <leigh.macdonald@gmail.com>
2026-08-17 15:32:54 -0600

    Convert to justfile

---
+++ shell.nix
@@ -0,0 +1,27 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-26.05";
+  pkgs = import nixpkgs {
+    config = {};
+    overlays = [];
+  };
+in
+  pkgs.mkShell {
+    shellHook = ''
+      export PATH="$PWD/frontend/node_modules/.bin:$PATH"
+    '';
+    buildInputs = with pkgs; [
+      go
+      golangci-lint
+      goreleaser
+      nilaway
+      just
+      just-lsp
+      nil
+      nixd
+      govulncheck
+      air
+      delve
+      markdownlint-cli2
+      sql-formatter
+    ];
+  }
